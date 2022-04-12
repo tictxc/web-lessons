1bbff61d23db55bd637fc32583c740685c93243f
tictxc <dmagarovskiy@gmail.com>
2022-04-12 13:40:51 +0300

    12.04.22

---
+++ 11/script.js
@@ -0,0 +1,20 @@
+function task1()
+{
+    document.write('Який-небудь текст');
+}
+function task2()
+{
+    document.write('<i>Який-небудь текст<i/>');
+}
+function task3()
+{
+    let str = 'text'
+    document.write('<i>' + str + '<i/>');
+}
+function task4()
+{
+    for(let i = 1; i <= 5;i++)
+    {
+        document.write(`${i}<br>`);
+    }
+}
